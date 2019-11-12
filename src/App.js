eb68e1d90982e79654e1422f9fb9516dfa927dc7
Muhammad Hassam <m.hassam@goldevelopers.com>
2019-11-12 08:46:49 +0500

    AR Kit ready

--- src/App.js
+++ src/App.js
@@ -3,30 +3,16 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import ShowBox from '../ShowBoxNativeView'
 
 class App extends React.Component {
-    // showModules=()=> {
-    //     return (
-    //         <View style={{ justifyContent: 'center', alignContent: 'center', backgroundColor:'black'}}>
-
-    //             <ShowBox style={{width:10, height:120}}/>
-
-    //         </View>
-    //     )
-    // }
     render() {
+        console.disableYellowBox=true;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
-                {/* <TouchableOpacity onPress={() => this.showModules()}> */}
                 <Text>
                     Prees Here to see Native UI
                 </Text>
-                {/* </TouchableOpacity> */}
-                <View style={{ justifyContent: 'center', alignContent: 'center', alignSelf: 'center', marginTop:20 }}>
-
-                    <ShowBox exampleProp="Hassam" style={{ width: 120, height: 50}} />
-
+                <View style={{ justifyContent: 'center', alignContent: 'center', alignSelf: 'center', marginTop: 20 }}>
+                    <ShowBox exampleProp="Hassam" style={{ width: 120, height: 50 }} />
                 </View>
-
-
             </View>
         )
     }
